5e87235d16e987fbb2abe5fc044f7286d3be481c
Eric Hebert <ayebear@gmail.com>
2017-04-18 10:19:39 -0400

    Added "show" callback to MenuState This will hide menu items when show returns false Added some examples

--- client/js/main_menu.js
+++ client/js/main_menu.js
@@ -1,8 +1,21 @@
+let river = false
+let shop = false
+
 let continueItem = [{text: "Go back", onclick: () => {
 	states.pop()
 }}]
 
-let continueState = new MenuState("continueState", continueItem)
+let continueState = new MenuState("continueState", [
+	{text: "Go near river", onclick: () => {
+		river = true
+	}},
+	{text: "Go near shop", onclick: () => {
+		shop = true
+	}},
+	{text: "Go back", onclick: () => {
+		states.pop()
+	}}
+])
 let checkSuppliesState = new MenuState("checkSuppliesState", continueItem)
 let mapState = new MenuState("mapState", continueItem)
 let changePaceState = new MenuState("changePaceState", continueItem)
@@ -10,7 +23,8 @@ let changeFoodState = new MenuState("changeFoodState", continueItem)
 let restState = new MenuState("restState", continueItem)
 let tradeState = new MenuState("tradeState", continueItem)
 let talkState = new MenuState("talkState", continueItem)
-let shopState = new MenuState("shopState", continueItem)
+let shopState = new MenuState("Welcome to the shop. What would you like?", continueItem)
+let fishState = new MenuState("You went fishing!", continueItem)
 
 let mainMenu = new MenuState("Welcome to Oregon Trail", [
 	{text: "Continue on trail", next: continueState},
@@ -24,5 +38,11 @@ let mainMenu = new MenuState("Welcome to Oregon Trail", [
 	}},
 	{text: "Attempt to trade", next: tradeState},
 	{text: "Talk to people", next: talkState},
-	{text: "Buy Supplies", next: shopState}
+	{text: "Buy Supplies", next: shopState, show: () => {
+		// TODO: Based on current location, you'd lookup in some table what things are there, such as a river
+		return shop
+	}},
+	{text: "Go Fishing", next: fishState, show: () => {
+		return river
+	}}
 ])
