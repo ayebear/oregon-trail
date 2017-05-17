87b9b78aea38fe89ed27584b3fc78983d955fb0e
Eric Hebert <ayebear@gmail.com>
2017-05-16 22:28:29 -0400

    Reverted game menu

--- client/js/game/game_menu.js
+++ client/js/game/game_menu.js
@@ -39,7 +39,7 @@ function getPartyHealth() {
 
 // The main game menu, which can be returned to during travel
 let gameMenu = new MenuState("What would you like to do?", [
-	{text: "Continue On Trail", next: new RiverGameState(), onclick: () => {locations.atShop = false}},
+	{text: "Continue On Trail", next: new TravelingState(), onclick: () => {locations.atShop = false}},
 	{text: "Check Supplies", onclick: () => {
 		states.push(new ContinueState(getSupplies()));
 	}},
